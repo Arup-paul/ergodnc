4b86323e36bdf0b07bb0a39ae3d7121acfc185c2
arup-paul <aruppaulctg@gmail.com>
2021-12-19 12:46:07 +0600

    Allow choosing a photo to become the featured data

--- tests/Feature/OfficeControllerTest.php
+++ tests/Feature/OfficeControllerTest.php
@@ -334,6 +334,57 @@ class OfficeControllerTest extends TestCase
 
     }
 
+
+    /**
+     * @test
+     */
+    public function itUpdatedTheFeaturedImageOfAnOffice()
+    {
+        $user =User::factory()->createQuietly();
+
+        $office  =   Office::factory()->for($user)->create();
+
+        $image = $office->images()->create([
+            'path' => 'image.jpg'
+        ]);
+        $this->actingAs($user);
+
+        $response = $this->putJson('/api/offices/'.$office->id,  [
+            'featured_image_id' => $image->id,
+        ]);
+
+        $response->assertOk()
+               ->assertJsonPath('data.featured_image_id',$image->id);
+
+
+
+    }
+
+    /**
+     * @test
+     */
+    public function itDoesntUpdateFeatureImageThatBelongsToAnotherOffice()
+    {
+        $user =User::factory()->create();
+
+        $office  =   Office::factory()->for($user)->create();
+        $office2  =   Office::factory()->for($user)->create();
+
+        $image = $office2->images()->create([
+            'path' => 'image.jpg'
+        ]);
+        $this->actingAs($user);
+
+        $response = $this->putJson('/api/offices/'.$office->id,  [
+            'featured_image_id' => $image->id,
+        ]);
+
+        $response->assertUnprocessable()->assertInvalid('featured_image_id');
+
+
+
+    }
+
     /**
      * @test
      */
